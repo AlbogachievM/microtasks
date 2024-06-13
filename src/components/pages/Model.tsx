4be48f7cd7b5d9ed25cd65f88c76bc9fab4906da
AlbogachievM <albogachiev-2014@mail.ru>
2024-06-13 16:35:18 +0300

    fix 8 tasks

--- src/components/pages/Model.tsx
+++ src/components/pages/Model.tsx
@@ -1,33 +1,53 @@
-import React from 'react';
-import {useParams} from "react-router-dom";
 import {adidasArr, AdidasItem} from "./Adidas";
+import {useParams} from "react-router-dom";
 import {pumaArr, PumaItem} from "./Puma";
 
-type CrossModelType = {
-    [key: string]: (AdidasItem[] | PumaItem[])
+
+
+type CrossModels= {
+    [key: string]: (AdidasItem[] | PumaItem[]);
 }
-const crossModel: CrossModelType = {
-    adidas: adidasArr,
+
+const crossModels: CrossModels={
+    adidas:adidasArr,
     puma: pumaArr
 }
+
 export const Model = () => {
-    const {id, model} = useParams()
+    // const params=useParams()
+    const { model, id } = useParams();
     console.log(model)
-    const currentModel = model && crossModel[model].find(el => el.id === Number(id))
+        //const currentModel=adidasArr.find(el=>el.id===Number(id))
+    const currentModel = model
+        ? crossModels[model].find((el) => el.id === Number(id))
+        : null;
 
-
-    return (
-        <div>
-            {currentModel ?
-                <>
-                    <h1>{currentModel.model}</h1>
-                    <span>{currentModel.collection}</span>
-                    <span>{currentModel.price}</span>
-                    <img src={currentModel.picture} alt={currentModel.model}/>
+      return (
+        <div style={{textAlign:'center'}}>
+            {currentModel
+                ?<>
+                    <h2>{currentModel?.model}</h2>
+                    <h4>{currentModel?.collection}</h4>
+                    <h3>{currentModel?.price}</h3>
+                    <img
+                        src={currentModel?.picture}
+                        alt={currentModel?.model}
+                        style={{width: '600px', height: 'auto', marginRight: '10px'}}
+                    />
                 </>
-                : <h2>Такой модели не существует</h2>
+                :   <h2>Модель отсутствует</h2>
             }
-        </div>
-    );
-};
 
+
+
+        {/*    <h2>{adidasArr[Number(params.id)].model}</h2>*/}
+        {/*    <h4>{adidasArr[Number(params.id)].collection}</h4>*/}
+        {/*    <h3>{adidasArr[Number(params.id)].price}</h3>*/}
+        {/*    <img*/}
+        {/*        src={adidasArr[Number(params.id)].picture}*/}
+        {/*        alt={adidasArr[Number(params.id)].model}*/}
+        {/*        style={{width: '600px', height: 'auto', marginRight: '10px'}}*/}
+        {/*    />*/}
+        </div>
+);
+};
--- src/components/pages/Puma.tsx
+++ src/components/pages/Puma.tsx
@@ -1,36 +1,37 @@
 import React from 'react';
-import pumaModel1 from "../../assets/AdiFOM_TRXN_Shoes_Black_IG7453_01_standard.webp";
-import pumaModel2 from "../../assets/Superstar_XLG_Shoes_Black_IG9777_01_standard.webp";
-import pumaModel3
-    from "../../assets/PostMove_Mid_Cloudfoam_Super_Lifestyle_Basketball_Mid_Classic_Shoes_Black_GY7163_01_standard.webp";
+
+import pumaModel1 from './../../assets/puma/kedy-puma-suede-classic-xxi-374915-01-20-1000x800.jpg'
+import pumaModel2 from './../../assets/puma/krossovki-puma-trinity-open-road-393361-02-3-1000x800.jpg'
+import pumaModel3 from './../../assets/puma/krossovki-puma-trinity-mid-hybrid-leather-393985-02-3-1000x800.jpg'
 import {Link} from "react-router-dom";
-import {adidasArr} from "./Adidas";
+
+
 
 export type PumaItem = {
-    id: number
+    id:number;
     model: string;
     collection: string;
     price: string;
     picture: string;
 }
+
 export const pumaArr: PumaItem[] = [
     {
-        id: 1,
+        id:1,
         model: 'PUMA ADIFOM TRXN',
         collection: 'new collection1',
         price: '100200$',
         picture: pumaModel1,
-
     },
     {
-        id: 2,
+        id:2,
         model: 'PUMA ADIFOM SUPER',
         collection: 'new collection22',
         price: '200300$',
         picture: pumaModel2
     },
     {
-        id: 3,
+        id:3,
         model: 'PUMA SUPER SUPERSKI',
         collection: 'new collection333',
         price: '300400$',
@@ -38,18 +39,21 @@ export const pumaArr: PumaItem[] = [
     }
 ]
 
-
 export const Puma = () => {
     return (
         <div>
             <h2>PUMA</h2>
-            {pumaArr.map((el, i) => (
-                <Link key={i} to={`/puma/${el.id}`}>
-                    <img src={el.picture}
-                         alt={el.model}
-                         style={{width: '350px', height: 'auto', marginRight: '10px'}}/>
-                </Link>
-            ) )}
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                {pumaArr.map((puma, index) => (
+                    <Link key={puma.id} to={`/puma/${puma.id}`}>
+                        <img
+                            src={puma.picture}
+                            alt={puma.model}
+                            style={{width: '200px', height: 'auto', marginRight: '10px'}}
+                        />
+                    </Link>
+                ))}
+            </div>
             <p>
                 What is Lorem Ipsum?
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
